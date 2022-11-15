aab1fb7b5c286548559126a3b8bf37fbdd467f43
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-11-15 04:13:21 -0300

    setting up eslint as linter and establishing mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
-
-app.listen(3001, () => {
-  console.log('🚀 server is running at: http://localhost:3001');
-});
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    app.listen(3001, () => {
+      console.log('🚀 server is running at: http://localhost:3001');
+    });
+    console.log('✅ connected to mongo');
+  })
+  .catch(() => console.log('⛔ failed to connect to mongo'));
